04a40b33ffb7757148f67b33cb822b6c51f7f91b
rivermont <william.11bennett@gmail.com>
2018-07-17 14:54:13 -0400

    Worked through tutorial 03.

--- main.py
+++ main.py
@@ -2,24 +2,67 @@ import pygame
 
 import constants
 
+
+#   ____ _
+#  / ___| | __ _ ___ ___  ___  ___
+# | |   | |/ _` / __/ __|/ _ \/ __|
+# | |___| | (_| \__ \__ \  __/\__ \
+#  \____|_|\__,_|___/___/\___||___/
+
 # Object
 # --> Item
 # --> Tile
-#     --> SolidTile
 # --> Entity
 #     --> Player
 #     --> NPC
 #         --> Shopkeep
 #     --> Monster
 
-
-class Object():
+class Object:
+    """The base object class for everything in the game."""
     pass
 
 
 class Item(Object):
+    """A wrapper class for items in the game."""
     pass
 
+
+class Tile(Object):
+    """The main class for all tiles in the game.
+    """
+    def __init__(self, passable=True, block_sight=False):
+        self.passable = passable
+        self.block_sight = block_sight
+
+
+class Entity(Object):
+    pass
+
+
+class Player(Entity):
+    pass
+
+
+class NPC(Entity):
+    pass
+
+
+class Monster(Entity):
+    pass
+
+
+class Shopkeep(NPC):
+    pass
+
+
+#  _____                   _
+# (____ \                 (_)
+#  _   \ \ ____ ____ _ _ _ _ ____   ____
+# | |   | / ___) _  | | | | |  _ \ / _  |
+# | |__/ / |  ( ( | | | | | | | | ( ( | |
+# |_____/|_|   \_||_|\____|_|_| |_|\_|| |
+#                                 (_____|
 
 def draw_window():
     global SURFACE_MAIN
@@ -35,6 +78,33 @@ def draw_window():
     # Update display
     pygame.display.flip()
 
+
+def draw_map(map_):
+    pass
+
+
+# ___  ___
+# |  \/  |
+# | .  . | __ _ _ __
+# | |\/| |/ _` | '_ \
+# | |  | | (_| | |_) |
+# \_|  |_/\__,_| .__/
+#              | |
+#              |_|
+
+class Map:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.map = [[Tile() for y in range(0, height)] for x in width]
+
+
+#   ______
+#  / _____)
+# | /  ___  ____ ____   ____
+# | | (___)/ _  |    \ / _  )
+# | \____/( ( | | | | ( (/ /
+#  \_____/ \_||_|_|_|_|\____)
 
 def game_main_loop():
 
@@ -75,6 +145,16 @@ def game_init():
 
     SURFACE_MAIN = pygame.display.set_mode((constants.GAME_WIDTH, constants.GAME_HEIGHT))
 
+
+#  ██▀███      █    ██     ███▄    █
+# ▓██ ▒ ██▒    ██  ▓██▒    ██ ▀█   █
+# ▓██ ░▄█ ▒   ▓██  ▒██░   ▓██  ▀█ ██▒
+# ▒██▀▀█▄     ▓▓█  ░██░   ▓██▒  ▐▌██▒
+# ░██▓ ▒██▒   ▒▒█████▓    ▒██░   ▓██░
+# ░ ▒▓ ░▒▓░   ░▒▓▒ ▒ ▒    ░ ▒░   ▒ ▒
+#   ░▒ ░ ▒░   ░░▒░ ░ ░    ░ ░░   ░ ▒░
+#   ░░   ░     ░░░ ░ ░       ░   ░ ░
+#    ░           ░                 ░
 
 if __name__ == '__main__':
     game_init()
